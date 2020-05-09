18a52f29a536a1b49294ab74cc2bb5556e9e9bef
dashurpa <dashurpa@dashurpas-MacBook-Pro.local>
2020-05-09 17:36:45 -0400

    ga plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,13 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "G-V3GJXB1MFE",
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
