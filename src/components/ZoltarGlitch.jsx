8fccaea0eeaed8766f65e35739c525d78aa59460
dashurpa <dashurpa@dashurpas-MacBook-Pro.local>
2020-05-09 16:24:59 -0400

    add glitch

---
+++ src/components/ZoltarGlitch.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import "./zoltarGlitch.css"
+
+const ZoltarGlicth = () => (
+  <>
+    <link
+      href="https://fonts.googleapis.com/css?family=Varela"
+      rel="stylesheet"
+      type="text/css"
+    />
+
+    <div class="glitch" data-text="ZOLTAR">
+      ZOLTAR
+    </div>
+  </>
+)
+export default ZoltarGlicth
